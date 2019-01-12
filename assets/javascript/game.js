d08918ce97a4b10f9ded3eed56c3de8385ba636b
Joe Gonzalez <joseph.gonzalez111@gmail.com>
2019-01-11 19:01:56 -0800

    updated hw#3.

---
+++ assets/javascript/game.js
@@ -0,0 +1,91 @@
+$(document).ready(function(){
+    // the problem originally was the DOM wasnt ready. Which is basically the html hadnt been formed yet so when it was trying to find the id that i had created it wasnt there yet. The doc.ready says dont load any of this until the document has loaded.
+    // a way to get around that would be to place the JS at the bottom of the file instead of in the head.
+    
+     // var compNumber = Math.floor(Math.random() * 4)+1;
+
+     var ltrChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",];
+    
+    var wins = 0
+    var losses = 0
+    var guessesLeft = 9
+    var lettersGuessed = []
+
+    
+      
+    
+
+
+      
+    var computerPick = ltrChoices[Math.floor(Math.random() * ltrChoices.length)];
+    console.log(computerPick);
+    function buttonHandler(){
+        var userClickedVal = $(this).val();
+        
+            if(guessesLeft !== 0){
+                //we keep playing
+                console.log('keep playing')
+            if (userClickedVal === computerPick) {
+                console.log('win')
+
+                wins++;
+                lettersGuessed = [];
+                guessesLeft = 9;
+            }
+            else {
+                console.log('bad guess')
+
+                guessesLeft--;
+                lettersGuessed.push(userClickedVal);
+            }
+        }
+        else {
+            console.log('lost')
+
+            //we lose
+            losses++;
+        }
+
+
+    };
+    $('button').click(buttonHandler);
+
+
+    // $('#computerPick').text(compLetter)
+
+    // then you need to pass in player choice to the function
+
+    // function buttonHandler(event){
+    //     if (compLetter === parseInt(event.currentTarget.value)){
+    //         $('#result_text').text('You guessed it! The Force is Strong in You!');
+    //     }else $('#result_text').text('You\'re just wrong!')
+    //     compLetter = Math.floor(Math.random() * 4)+1;
+    // }
+
+
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+                /*
+                $("#button_1").unbind('click').bind('click', function() {
+                    buttonHandler(1);
+                });
+                ("#button_2").unbind('click').bind('click', function() {
+                    buttonHandler(2);
+                }); 
+                $("#button_3").unbind('click').bind('click', function() {
+                    buttonHandler(3);
+                }); 
+                $("#button_4").unbind('click').bind('click', function() {
+                    buttonHandler(4);
+                });
+                */
+
+})
